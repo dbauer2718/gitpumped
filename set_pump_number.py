54d9a00aa1e104cd4978e44941f5f1c66e985738
dbauer2718 <dbauer2718@gmail.com>
2020-08-11 12:35:33 -0400

    fixed set_pump_number.py

--- set_pump_number.py
+++ set_pump_number.py
@@ -9,16 +9,16 @@ pump_number = 1
 ### LEAVE EVERYTHING BELOW HERE THE SAME
 def print_pump_number(ser,tot_range=100):
     for i in range(tot_range):
-        ser.write('%iADR\x0D'%i)
+        ser.write(b'%iADR\x0D'%i)
         output = ser.readline()
         if len(output)>0:
-            print 'current pump set to %i'%i
+            print('current pump set to %i'%i)
             break
 
 ser = serial.Serial(serial_port,19200,timeout=0.1)
 print_pump_number(ser)
-print 'setting pump to %i'%pump_number
-ser.write('*ADR%iB19200\x0D'%pump_number)
+print('setting pump to %i'%pump_number)
+ser.write(b'*ADR%iB19200\x0D'%pump_number)
 ser.readline()
 print_pump_number(ser)
 ser.close()
